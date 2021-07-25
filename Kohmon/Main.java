609057b3b91a9ca23205c6ba265c82a6b6424fff
kuccho524 <kucchachachanocha@gmail.com>
2021-07-25 15:20:19 +0900

    Create Kohmon

---
+++ Kohmon/Main.java
@@ -0,0 +1,15 @@
+import comment.Zenhan;
+
+public class Main { 
+
+  // mainメソッドを定義
+  public static void main(String[] args) throws Exception {
+
+    // 各クラスを呼び出す。
+    Zenhan.doWarusa();
+    Zenhan.doTogame();
+    comment.Kouhan.callDeae();
+    comment.Kouhan.showMondokoro();
+  }
+
+}
---
+++ Kohmon/Zenhan.java
@@ -0,0 +1,15 @@
+package comment;
+
+public class Zenhan {
+
+  // doWarusaクラスを定義
+  public static void doWarusa() {
+    System.out.println("きなこでござる。食えませんがの。");
+  }
+
+  // doTogameクラスを定義
+  public static void doTogame() {
+    System.out.println("この老いぼれの目はごまかせませんぞ。");
+  }
+
+}
