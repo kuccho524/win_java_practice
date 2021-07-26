1da198ff3dc2591695009dfab81dd84e6c682784
kuccho524 <kucchachachanocha@gmail.com>
2021-07-26 12:00:25 +0900

    Create hello

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+  public static void main(String[] args){
+    System.out.println("Hello!");
+    System.out.print('H');
+    System.out.print('e');
+    System.out.print('l');
+    System.out.print('l');
+    System.out.print('o');
+    System.out.print('!');
+  }
+}
