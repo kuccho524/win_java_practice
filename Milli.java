0fd9babd43feb73eecb43d21e3e4c8a5e23eaf99
kuccho524 <kucchachachanocha@gmail.com>
2021-08-04 18:18:27 +0900

    Create milli

---
+++ Milli.java
@@ -0,0 +1,12 @@
+public class Milli {
+  public static void main(String[] args) {
+    int result = 0;
+    long start = System.currentTimeMillis();
+
+    for(int i = 0; i < 10000000; i++) {
+      result += i;
+    }
+    long end = System.currentTimeMillis();
+    System.out.println((end - start) + " ms");
+  }
+}
