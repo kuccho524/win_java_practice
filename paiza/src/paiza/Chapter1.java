24228ad24bcdfaad313bf07e4f51168540fbf21e
kuccho524 <kucchachachanocha@gmail.com>
2021-10-13 12:33:57 +0900

    chapter1!

---
+++ paiza/src/paiza/Chapter1.java
@@ -0,0 +1,17 @@
+package paiza;
+
+/*
+- プログラムでは、命令を使って、コンピュータに動作を指示します
+- System.out.println は、データを表示する命令です。この命令をメソッドと呼びます
+- カッコで指定するデータを「引数」と呼びます
+- 表示する文字データをダブルクォーテーションで囲みます
+- コードの行末に「;」か「}」
+*/
+
+public class Chapter1 {
+	public static void main(String[] args) {
+		// 「Hello World」、「Hello Paiza」を２行で表示する
+		System.out.println("Hello World");
+		System.out.println("Hello Paiza");
+	}
+}
