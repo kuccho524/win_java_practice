c2cf07d1b087f6e80d08ac4267432bd410c82549
kuccho524 <kucchachachanocha@gmail.com>
2021-10-14 15:51:29 +0900

    kadai10

---
+++ paiza/src/paiza/kadai/Kadai10.java
@@ -0,0 +1,21 @@
+package paiza.kadai;
+
+/*
+ * 下記の状態から変数を使って「Hello world」を表示させる
+ * public static void main(String[] args) {
+        String message = "";
+        System.out.println(message);
+    }
+ */
+
+public class Kadai10 {
+	public static void main(String[] args) {
+		// Stsrin型を持つ変数messageに「Hello paiza」を代入
+		String message = "Hello paiza";
+
+		// 変数messageに代入されているものを２行表示する
+		System.out.println(message);
+		System.out.println(message);
+
+	}
+}
