655360549ccbae98fe4a23ed9a2267601e5da5a1
kuccho524 <kucchachachanocha@gmail.com>
2021-10-14 15:14:11 +0900

    kadai4

---
+++ paiza/src/paiza/kadai/Kadai4.java
@@ -0,0 +1,18 @@
+package paiza.kadai;
+
+/*
+ * 以下の初期値からコメントアウトとして、「Hello Java」を出力する
+ * public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        System.out.println("Hello paiza");
+    }
+}
+ */
+
+public class Kadai4 {
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+        // System.out.println("Hello paiza");
+	}
+}
