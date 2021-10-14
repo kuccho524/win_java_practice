d4f301d46174ef17c7bbcc20c3c0afbd3c85367a
kuccho524 <kucchachachanocha@gmail.com>
2021-10-14 15:45:57 +0900

    chapter3

---
+++ paiza/src/paiza/Chapter3.java
@@ -0,0 +1,29 @@
+package paiza;
+
+/*
+ * 変数とは、データの入れ物
+ * 変数名の付け方
+ * １文字目
+ * ・アルファベット
+ * ・アンダーバー
+ *
+ * ２文字目
+ * ・アルファベット
+ * ・アンダーバー
+ * ・数字
+ */
+
+public class Chapter3 {
+	public static void main(String[] args) {
+		// String型を持つ変数greetingに「Hello paiza」を代入
+        String greeting = "Hello paiza";
+
+        // greetingに入っている値を表示する
+		System.out.println(greeting);
+		System.out.println(greeting);
+
+		// int型を持つ変数numberに100を代入する
+		int number = 100;
+		System.out.println(number);
+    }
+}
