e6d667c8680b5bc440c0aaa8c9b76b17021d1f7a
kuccho524 <kucchachachanocha@gmail.com>
2021-10-14 15:19:32 +0900

    chapter2

---
+++ paiza/src/paiza/Chapter2.java
@@ -0,0 +1,14 @@
+package paiza;
+
+public class Chapter2 {
+	public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("100 + 30");
+        System.out.println(100);
+    }
+}
+
+/*
+ * 数値入力の際、「"」で囲う ⇒ 文字列として判断される
+ * 「"」で囲わない ⇒ 数値として判断される
+ */
