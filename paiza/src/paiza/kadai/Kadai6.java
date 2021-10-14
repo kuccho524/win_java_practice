23e62f1f5c15e00e63e406ae32fddbefac44f660
kuccho524 <kucchachachanocha@gmail.com>
2021-10-14 15:27:42 +0900

    kadai6

---
+++ paiza/src/paiza/kadai/Kadai6.java
@@ -0,0 +1,13 @@
+package paiza.kadai;
+
+/*
+ * 1つめの文で「12345 + 6789」の和を表示し、
+ * 2つめの文で「12345 + 6789」を表示する
+ */
+
+public class Kadai6 {
+	public static void main(String[] args) {
+		System.out.println(12345 + 6789);
+		System.out.println("12345 + 6789");
+	}
+}
