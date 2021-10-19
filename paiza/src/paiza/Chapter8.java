15a599c254acf2fc59e5f9b97b473a07289e9b09
kuccho524 <kucchachachanocha@gmail.com>
2021-10-19 16:28:38 +0900

    Chaoter8

---
+++ paiza/src/paiza/Chapter8.java
@@ -0,0 +1,11 @@
+package paiza;
+
+public class Chapter8 {
+	public static void main(String[] args) {
+		String greeting = "Hello paiza";
+
+        for (int i = 0; i < 5; i++) {
+        	System.out.println(greeting + ":" + i);
+        }
+	}
+}
