27ad0844bd09740526994db2080a5dc48c9ab8d4
kuccho524 <kucchachachanocha@gmail.com>
2021-10-21 08:48:54 +0900

    Chapter11

---
+++ paiza/src/paiza/Chapter11.java
@@ -0,0 +1,9 @@
+package paiza;
+
+public class Chapter11 {
+	public static void main(String[] args) {
+		System.out.println("<h1>hello world</h1>");
+		System.out.print("<p>世界の皆さん、");
+		System.out.println("<b>こんにちは</b></p>");
+	}
+}
