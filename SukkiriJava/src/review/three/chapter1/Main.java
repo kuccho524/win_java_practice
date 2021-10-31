ca04dee73fbbf8ff28eb760505a02f246b594fbe
kuccho524 <kucchachachanocha@gmail.com>
2021-10-31 21:17:15 +0900

    review chapter1

---
+++ SukkiriJava/src/review/three/chapter1/Main.java
@@ -0,0 +1,31 @@
+package review.three.chapter1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 * 変数aに3、変数bに5を代入し、掛け算の結果を求める
+		 */
+		int a = 3;
+		int b = 5;
+		System.out.println(a * b);
+
+
+		/*
+		 * 以下を適切なデータ型で初期化する
+		 * ①true
+		 * ②'駆'
+		 * ③3.14
+		 * ④314159265853979L
+		 * ⑤"ミナトの攻撃！敵に15ポイントのダメージを与えた"
+		 */
+
+		boolean boo = true;
+		char c = '駆';
+		double pi = 3.14;
+		long l = 314159265853979L;
+		String msg = "ミナトの攻撃！敵に15ポイントのダメージを与えた";
+
+	}
+
+}
